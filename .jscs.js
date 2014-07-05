ed6e3f54927e5371730fb616292b1810d1d4331f
andrewblond <andrewblond@yandex.com>
2014-07-05 13:37:40 +0400

    Used js format for jscs config

---
+++ .jscs.js
@@ -0,0 +1,48 @@
+module.exports = {
+    excludeFiles: [
+        'node_modules/**',
+        'coverage/**'
+    ],
+    requireCurlyBraces: ['if', 'else', 'for', 'while', 'do', 'try', 'catch', 'case', 'default'],
+    requireSpaceAfterKeywords: ['if', 'else', 'for', 'while', 'do', 'switch', 'return', 'try', 'catch'],
+    requireSpaceBeforeBinaryOperators: [
+        '=', '+=', '-=', '*=', '/=', '%=', '<<=', '>>=', '>>>=',
+        '&=', '|=', '^=', '+=',
+
+        '+', '-', '*', '/', '%', '<<', '>>', '>>>', '&',
+        '|', '^', '&&', '||', '===', '==', '>=',
+        '<=', '<', '>', '!=', '!=='
+    ],
+    requireSpacesInConditionalExpression: true,
+    requireSpaceAfterBinaryOperators: true,
+    disallowSpacesInsideParentheses: true,
+    disallowSpaceBeforePostfixUnaryOperators: true,
+    disallowSpaceAfterPrefixUnaryOperators: true,
+    disallowSpaceBeforeBinaryOperators: [',', ':'],
+    disallowKeywords: ['with'],
+    disallowKeywordsOnNewLine: ['else', 'catch'],
+    requireSpacesInFunctionExpression: {
+        beforeOpeningCurlyBrace: true
+    },
+    requireSpacesInAnonymousFunctionExpression: {
+        beforeOpeningRoundBrace: true
+    },
+    disallowSpacesInNamedFunctionExpression: {
+        beforeOpeningRoundBrace: true
+    },
+
+    disallowYodaConditions: true,
+    safeContextKeyword: '_this',
+    requireCapitalizedConstructors: true,
+    requireCommaBeforeLineBreak: true,
+
+    disallowEmptyBlocks: true,
+    disallowMultipleLineBreaks: true,
+    disallowMixedSpacesAndTabs: true,
+    disallowTrailingWhitespace: true,
+
+    validateQuoteMarks: '\'',
+    maximumLineLength: 120,
+    requireLineFeedAtFileEnd: true,
+    validateLineBreaks: 'LF'
+};
