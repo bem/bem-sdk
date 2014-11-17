9782c96a64ef968c4aeb68c3aa97f330246fee18
andrewblond <andrewblond@yandex.com>
2014-11-17 16:04:33 +0300

    Updated tests

--- test/normalize-1.0.test.js
+++ test/normalize-1.0.test.js
@@ -1,37 +1,56 @@
 var nomralize = require('../lib/normalize-v1');
 
 describe('nomralize 1.0', function () {
+    it('must support undefined', function () {
+        nomralize().should.eql([]);
+    });
 
-    it('should support objects', function () {
-        nomralize({ name: 'block' }, '1.0').should.eql([
+    it('must support empty array', function () {
+        nomralize([]).should.eql([]);
+    });
+
+    it('must support objects', function () {
+        nomralize({ name: 'block' }).should.eql([
             { block: 'block' }
         ]);
     });
 
-describe('mods', function () {
-    it('should support objects', function () {
-        nomralize({ name: 'block', mods: [{ name: 'mod', vals: [{ name: 'val' }] }]}, '1.0').should.eql([
-            { block: 'block' },
-            { block: 'block', modName: 'mod', modVal: 'val' }
-        ]);
-    });
-});
-
-describe('elems', function () {
-    it('should support arrays', function () {
-        nomralize({name: 'block', elems: [{name: 'elem-1'}, {name: 'elem-2'}]}, '1.0').should.eql([
-            {block: 'block'},
-            {block: 'block', elem: 'elem-1'},
-            {block: 'block', elem: 'elem-2'}
+    it('must support array', function () {
+        nomralize([{ name: 'A' }, { name: 'B' }]).should.eql([
+            { block: 'A' }, { block: 'B' }
         ]);
     });
 
-    it('should support objects', function () {
-        nomralize({name: 'block', elems: [{name: 'elem', mods: [{name: 'mod', vals: [{ name: 'val'}]}]}]}, '1.0').should.eql([
-            {block: 'block'},
-            {block: 'block', elem: 'elem'},
-            {block: 'block', elem: 'elem', modName: 'mod', modVal: 'val'}
-        ]);
+    describe('mods', function () {
+        it('must support objects', function () {
+            nomralize({ name: 'block', mods: [{ name: 'mod', vals: [{ name: 'val' }] }] }).should.eql([
+                { block: 'block' },
+                { block: 'block', modName: 'mod', modVal: 'val' }
+            ]);
+        });
     });
-});
+
+    describe('elems', function () {
+        it('must support arrays', function () {
+            nomralize({
+                name: 'block',
+                elems: [{ name: 'elem-1' }, { name: 'elem-2' }]
+            }).should.eql([
+                { block: 'block' },
+                { block: 'block', elem: 'elem-1' },
+                { block: 'block', elem: 'elem-2' }
+            ]);
+        });
+
+        it('must support objects', function () {
+            nomralize({
+                name: 'block',
+                elems: [{ name: 'elem', mods: [{ name: 'mod', vals: [{ name: 'val' }] }] }]
+            }).should.eql([
+                { block: 'block' },
+                { block: 'block', elem: 'elem' },
+                { block: 'block', elem: 'elem', modName: 'mod', modVal: 'val' }
+            ]);
+        });
+    });
 });
--- test/normalize-2.0.test.js
+++ test/normalize-2.0.test.js
@@ -1,165 +1,149 @@
 var nomralize = require('../lib/normalize-v2');
 
 describe('nomralize 2.0', function () {
+    it('must support undefined', function () {
+        nomralize().should.eql([]);
+    });
 
-    it('should support objects', function () {
+    it('must support empty array', function () {
+        nomralize([]).should.eql([]);
+    });
+
+    it('must support block', function () {
         nomralize({ block: 'block' }).should.eql([
             { block: 'block' }
         ]);
     });
 
-describe('mods', function () {
-    it('should support objects', function () {
-        nomralize({ block: 'block', modName: 'mod' }).should.eql([
-            { block: 'block' },
-            { block: 'block', modName: 'mod', modVal: true }
+    it('must support array', function () {
+        nomralize([{ block: 'A' }, { block: 'B' }]).should.eql([
+            { block: 'A' }, { block: 'B' }
         ]);
     });
 
-    it('should support objects', function () {
-        nomralize({ block: 'block', modName: 'mod', modVal: true }).should.eql([
-            { block: 'block' },
-            { block: 'block', modName: 'mod', modVal: true }
-        ]);
+    describe('mods', function () {
+        it('must support shortcat for boolean mod by BEM-notation', function () {
+            nomralize({ block: 'block', modName: 'mod' }).should.eql([
+                { block: 'block' },
+                { block: 'block', modName: 'mod', modVal: true }
+            ]);
+        });
+
+        it('must support boolean mod by BEM-notation', function () {
+            nomralize({ block: 'block', modName: 'mod', modVal: true }).should.eql([
+                { block: 'block' },
+                { block: 'block', modName: 'mod', modVal: true }
+            ]);
+        });
+
+        it('must support mod by BEM-notation', function () {
+            nomralize({ block: 'block', modName: 'mod', modVal: 'val' }).should.eql([
+                { block: 'block' },
+                { block: 'block', modName: 'mod', modVal: 'val' }
+            ]);
+        });
+
+        it('must support mods as objects', function () {
+            nomralize({ block: 'block', mods: { mod: 'val' } }).should.eql([
+                { block: 'block' },
+                { block: 'block', modName: 'mod', modVal: 'val' }
+            ]);
+        });
+
+        it('must support boolean mods as array', function () {
+            nomralize({ block: 'block', mods: ['mod-1', 'mod-2'] }).should.eql([
+                { block: 'block' },
+                { block: 'block', modName: 'mod-1', modVal: true },
+                { block: 'block', modName: 'mod-2', modVal: true }
+            ]);
+        });
+
+        it('must support mod values as array', function () {
+            nomralize({ block: 'block', mods: { mod: ['val-1', 'val-2'] } }).should.eql([
+                { block: 'block' },
+                { block: 'block', modName: 'mod', modVal: 'val-1' },
+                { block: 'block', modName: 'mod', modVal: 'val-2' }
+            ]);
+        });
     });
 
-    it('should support objects', function () {
-        nomralize({ block: 'block', mod: 'mod' }).should.eql([
-            { block: 'block' },
-            { block: 'block', modName: 'mod', modVal: true }
-        ]);
+    describe('elems', function () {
+        it('must support elem by BEM-notation', function () {
+            nomralize({ block: 'block', elem: 'elem' }).should.eql([
+                { block: 'block' },
+                { block: 'block',  elem: 'elem' }
+            ]);
+        });
+
+        it('must support boolean mod of elem by BEM-notation', function () {
+            nomralize({ block: 'block', elem: 'elem', modName: 'mod', modVal: true }).should.eql([
+                { block: 'block' },
+                { block: 'block', elem: 'elem' },
+                { block: 'block', elem: 'elem', modName: 'mod', modVal: true }
+            ]);
+        });
+
+        it('must support elem mod by BEM-notation', function () {
+            nomralize({ block: 'block', elem: 'elem', modName: 'mod', modVal: 'val' }).should.eql([
+                { block: 'block' },
+                { block: 'block', elem: 'elem' },
+                { block: 'block', elem: 'elem', modName: 'mod', modVal: 'val' }
+            ]);
+        });
+
+        it('must support elems as arrays', function () {
+            nomralize({ block: 'block', elems: ['elem-1', 'elem-2'] }).should.eql([
+                { block: 'block' },
+                { block: 'block', elem: 'elem-1' },
+                { block: 'block',  elem: 'elem-2' }
+            ]);
+        });
+
+        it('must support elem mods as object', function () {
+            nomralize({ block: 'block', elem: 'elem', mods: { mod: 'val' } }).should.eql([
+                { block: 'block' },
+                { block: 'block', elem: 'elem' },
+                { block: 'block', elem: 'elem', modName: 'mod', modVal: 'val' }
+            ]);
+        });
+
+        it('must support boolean mods of elem as array', function () {
+            nomralize({ block: 'block', elem: 'elem', mods: ['mod-1', 'mod-2'] }).should.eql([
+                { block: 'block' },
+                { block: 'block', elem: 'elem' },
+                { block: 'block', elem: 'elem', modName: 'mod-1', modVal: true },
+                { block: 'block', elem: 'elem', modName: 'mod-2', modVal: true }
+            ]);
+        });
+
+        it('must support mod values of elem as array', function () {
+            nomralize({ block: 'block', elem: 'elem', mods: { mod: ['val-1', 'val-2'] } }).should.eql([
+                { block: 'block' },
+                { block: 'block', elem: 'elem' },
+                { block: 'block', elem: 'elem', modName: 'mod', modVal: 'val-1' },
+                { block: 'block', elem: 'elem', modName: 'mod', modVal: 'val-2' }
+            ]);
+        });
     });
 
-    it('should support objects', function () {
-        nomralize({ block: 'block', mod: 'mod', modVal: 'val' }).should.eql([
-            { block: 'block' },
-            { block: 'block', modName: 'mod', modVal: true }
-        ]);
-    });
+    describe('mix', function () {
+        it('must support elems with block mods', function () {
+            nomralize({ block: 'block', elems: ['elem-1', 'elem-2'], mods: ['mod-1', 'mod-2'] }).should.eql([
+                { block: 'block' },
+                { block: 'block', elem: 'elem-1' },
+                { block: 'block', elem: 'elem-2' },
+                { block: 'block', modName: 'mod-1', modVal: true },
+                { block: 'block', modName: 'mod-2', modVal: true }
+            ]);
+        });
 
-    it('should support objects', function () {
-        nomralize({ block: 'block', modName: 'mod', modVal: 'val' }).should.eql([
-            { block: 'block' },
-            { block: 'block', modName: 'mod', modVal: 'val' }
-        ]);
+        it('must support elems with block mod', function () {
+            nomralize({ block: 'block', elems: ['elem-1', 'elem-2'], modName: 'mod' }).should.eql([
+                { block: 'block' },
+                { block: 'block', elem: 'elem-1' },
+                { block: 'block', elem: 'elem-2' },
+                { block: 'block', modName: 'mod', modVal: true }
+            ]);
+        });
     });
-
-    it('should support objects', function () {
-        nomralize({ block: 'block', mods: { mod: 'val' }}).should.eql([
-            { block: 'block' },
-            { block: 'block', modName: 'mod', modVal: 'val' }
-        ]);
-    });
-
-    it('should support arrays as object values', function () {
-        nomralize({ block: 'block', mods: ['mod-1', 'mod-2'] }).should.eql([
-            { block: 'block' },
-            { block: 'block', modName: 'mod-1', modVal: true },
-            { block: 'block', modName: 'mod-2', modVal: true }
-        ]);
-    });
-
-    it('should support arrays as object values', function () {
-        nomralize({ block: 'block', mods: { mod: ['val-1', 'val-2'] }}).should.eql([
-            { block: 'block' },
-            { block: 'block', modName: 'mod', modVal: 'val-1' },
-            { block: 'block', modName: 'mod', modVal: 'val-2' }
-        ]);
-    });
-});
-
-describe('elems', function () {
-    it('should support arrays', function () {
-        nomralize({ block: 'block', elem: 'elem' }).should.eql([
-            { block: 'block' },
-            { block: 'block',  elem: 'elem' }
-        ]);
-    });
-
-    it('should support arrays', function () {
-        nomralize({ block: 'block', elems: ['elem-1', 'elem-2'] }).should.eql([
-            { block: 'block' },
-            { block: 'block', elem: 'elem-1' },
-            { block: 'block',  elem: 'elem-2' }
-        ]);
-    });
-
-    it('should support objects', function () {
-        nomralize({ block: 'block', elem: 'elem', mod: 'mod' }).should.eql([
-            { block: 'block' },
-            { block: 'block', elem: 'elem' },
-            { block: 'block', elem: 'elem', modName: 'mod', modVal: true }
-        ]);
-    });
-
-    it('should support objects', function () {
-        nomralize({ block: 'block', elem: 'elem', mod: 'mod', modVal: 'val' }).should.eql([
-            { block: 'block' },
-            { block: 'block', elem: 'elem' },
-            { block: 'block', elem: 'elem', modName: 'mod', modVal: true }
-        ]);
-    });
-
-    it('should support objects', function () {
-        nomralize({ block: 'block', elem: 'elem', modName: 'mod', modVal: 'val' }).should.eql([
-            { block: 'block' },
-            { block: 'block', elem: 'elem' },
-            { block: 'block', elem: 'elem', modName: 'mod', modVal: 'val' }
-        ]);
-    });
-
-    it('should support objects', function () {
-        nomralize({ block: 'block', elem: 'elem', mods: { mod: 'val' }}).should.eql([
-            { block: 'block' },
-            { block: 'block', elem: 'elem' },
-            { block: 'block', elem: 'elem', modName: 'mod', modVal: 'val' }
-        ]);
-    });
-
-    it('should support objects', function () {
-        nomralize({ block: 'block', elem: 'elem', mods: { mod: 'val' }}).should.eql([
-            { block: 'block' },
-            { block: 'block', elem: 'elem' },
-            { block: 'block', elem: 'elem', modName: 'mod', modVal: 'val' }
-        ]);
-    });
-
-    it('should support arrays as object values', function () {
-        nomralize({ block: 'block', elem: 'elem', mods: ['mod-1', 'mod-2'] }).should.eql([
-            { block: 'block' },
-            { block: 'block', elem: 'elem' },
-            { block: 'block', elem: 'elem', modName: 'mod-1', modVal: true },
-            { block: 'block', elem: 'elem', modName: 'mod-2', modVal: true }
-        ]);
-    });
-
-    it('should support arrays as object values', function () {
-        nomralize({ block: 'block', elem: 'elem', mods: { mod: ['val-1', 'val-2'] }}).should.eql([
-            { block: 'block' },
-            { block: 'block', elem: 'elem' },
-            { block: 'block', elem: 'elem', modName: 'mod', modVal: 'val-1' },
-            { block: 'block', elem: 'elem', modName: 'mod', modVal: 'val-2' }
-        ]);
-    });
-});
-
-    it('should support arrays as object values', function () {
-        nomralize({ block: 'block', elems: ['elem-1', 'elem-2'], mods: ['mod-1', 'mod-2'] }).should.eql([
-            { block: 'block' },
-            { block: 'block', elem: 'elem-1' },
-            { block: 'block', elem: 'elem-2' },
-            { block: 'block', modName: 'mod-1', modVal: true },
-            { block: 'block', modName: 'mod-2', modVal: true }
-        ]);
-    });
-
-    it('should support arrays as object values', function () {
-        nomralize({ block: 'block', elems: ['elem-1', 'elem-2'], mod: 'mod' }).should.eql([
-            { block: 'block' },
-            { block: 'block', elem: 'elem-1' },
-            { block: 'block', elem: 'elem-2' },
-            { block: 'block', modName: 'mod', modVal: true }
-        ]);
-    });
-
 });
