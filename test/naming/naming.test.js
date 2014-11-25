7c668ef81266d22da34048471a98de43286ac69c
andrewblond <andrewblond@yandex.com>
2014-11-25 13:28:01 +0300

    Added tests for naming

---
+++ test/naming/naming.test.js
@@ -0,0 +1,84 @@
+var mock = require('mock-fs'),
+    walk = require('../../lib/index');
+
+function assert(levels, naming, expected, cb) {
+    var buffer = [],
+        walker = walk(levels, { scheme: 'flat', naming: naming });
+
+    walker.on('data', function (obj) {
+        buffer.push(obj);
+    });
+
+    walker.on('end', function () {
+        try {
+            buffer.must.eql(expected);
+            cb();
+        } catch (e) {
+            cb(e);
+        }
+    });
+}
+
+describe('naming', function () {
+    afterEach(function () {
+        mock.restore();
+    });
+
+    it('must support original naming', function (done) {
+        mock({
+            blocks: {
+                'block__elem_bool-mod.ext': ''
+            }
+        });
+
+        assert(['blocks'], { elem: '__', mod: '_' }, [{
+            block: 'block',
+            elem: 'elem',
+            modName: 'bool-mod',
+            modVal: true,
+            tech: 'ext',
+            level: 'blocks',
+            path: 'blocks/block__elem_bool-mod.ext'
+        }], done);
+    });
+
+    it('must support Convention by Harry Roberts', function (done) {
+        mock({
+            blocks: {
+                'block__elem--bool-mod.ext': ''
+            }
+        });
+
+        assert(['blocks'], { elem: '__', mod: '--' }, [{
+            block: 'block',
+            elem: 'elem',
+            modName: 'bool-mod',
+            modVal: true,
+            tech: 'ext',
+            level: 'blocks',
+            path: 'blocks/block__elem--bool-mod.ext'
+        }], done);
+    });
+
+    it('must support custom naming', function (done) {
+        mock({
+            blocks: {
+                'block-elem--boolMod.ext': ''
+            }
+        });
+
+        assert(['blocks'], {
+            elem: '-',
+            mod: '--',
+            wordPattern: '[a-zA-Z0-9]+'
+        }, [{
+            block: 'block',
+            elem: 'elem',
+            modName: 'boolMod',
+            modVal: true,
+            tech: 'ext',
+            level: 'blocks',
+            path: 'blocks/block-elem--boolMod.ext'
+        }], done);
+    });
+});
