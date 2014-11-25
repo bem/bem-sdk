6c581aa6226ae3845c8abea838e14430a6257ef6
andrewblond <andrewblond@yandex.com>
2014-11-25 12:32:44 +0300

    Moved nested scheme to `schemes`

---
+++ schemes/index.js
@@ -0,0 +1,1 @@
+exports.nested = require('./nested');
---
+++ schemes/nested.js
@@ -0,0 +1,22 @@
+var path = require('path'),
+    naming = require('bem-naming'),
+    sep = path.sep;
+
+module.exports = function (item, add, walk, done) {
+    var basename = item.basename,
+        filename = item.filename,
+        parent = path.basename(item.dirname),
+        grandParent = path.basename(path.dirname(item.dirname)),
+        depth = filename.split(sep).length - item.level.split(sep).length;
+
+    if ((depth === 1 && naming.isBlock(item.notation)) ||
+        (depth === 2 && naming.isBlockMod(parent + basename)) ||
+        (depth === 2 && naming.isElem(parent + basename)) ||
+        (depth === 3 && naming.isElemMod(grandParent + parent + basename))
+    ) {
+        return walk(filename, done);
+    }
+
+    add(item);
+    done();
+};
