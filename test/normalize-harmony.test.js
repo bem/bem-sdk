77f0821ba810c2277af29635f9fc6cea4d65bdca
andrewblond <andrewblond@yandex.com>
2015-01-08 21:26:39 +0300

    Updated tests

--- test/normalize-harmony.test.js
+++ test/normalize-harmony.test.js
@@ -14,48 +14,83 @@ describe('normalize --harmony', function () {
     });
 
     it('must support block', function () {
-        normalize({ block: 'block' }).must.eql([
+        var entities = { block: 'block' };
+
+        normalize(entities).must.eql([
             { block: 'block' }
         ]);
     });
 
     it('must support array', function () {
-        normalize([{ block: 'A' }, { block: 'B' }]).must.eql([
-            { block: 'A' }, { block: 'B' }
+        var entities = [
+            { block: 'block-1' },
+            { block: 'block-2' }
+        ];
+
+        normalize(entities).must.eql([
+            { block: 'block-1' },
+            { block: 'block-2' }
         ]);
     });
 
     describe('mods', function () {
         it('must support shortcat for boolean mod by BEM-notation', function () {
-            normalize({ block: 'block', modName: 'mod' }).must.eql([
+            var entities = {
+                block: 'block',
+                modName: 'mod'
+            };
+
+            normalize(entities).must.eql([
                 { block: 'block' },
                 { block: 'block', modName: 'mod', modVal: true }
             ]);
         });
 
         it('must support boolean mod by BEM-notation', function () {
-            normalize({ block: 'block', modName: 'mod', modVal: true }).must.eql([
+            var entities = {
+                block: 'block',
+                modName: 'mod',
+                modVal: true
+            };
+
+            normalize(entities).must.eql([
                 { block: 'block' },
                 { block: 'block', modName: 'mod', modVal: true }
             ]);
         });
 
         it('must support mod by BEM-notation', function () {
-            normalize({ block: 'block', modName: 'mod', modVal: 'val' }).must.eql([
+            var entities = {
+                block: 'block',
+                modName: 'mod',
+                modVal: 'val'
+            };
+
+            normalize(entities).must.eql([
                 { block: 'block' },
                 { block: 'block', modName: 'mod', modVal: 'val' }
             ]);
         });
 
         it('must support mods as objects', function () {
-            normalize({ block: 'block', mods: { mod: 'val' } }).must.eql([
+            var entities = {
+                block: 'block',
+                mods: { mod: 'val' }
+            };
+
+            normalize(entities).must.eql([
                 { block: 'block' },
                 { block: 'block', modName: 'mod', modVal: 'val' }
             ]);
         });
 
         it('must support boolean mods as array', function () {
-            normalize({ block: 'block', mods: ['mod-1', 'mod-2'] }).must.eql([
+            var entities = {
+                block: 'block',
+                mods: ['mod-1', 'mod-2']
+            };
+
+            normalize(entities).must.eql([
                 { block: 'block' },
                 { block: 'block', modName: 'mod-1', modVal: true },
                 { block: 'block', modName: 'mod-2', modVal: true }
@@ -63,7 +98,12 @@ describe('normalize --harmony', function () {
         });
 
         it('must support mod values as array', function () {
-            normalize({ block: 'block', mods: { mod: ['val-1', 'val-2'] } }).must.eql([
+            var entities = {
+                block: 'block',
+                mods: { mod: ['val-1', 'val-2'] }
+            };
+
+            normalize(entities).must.eql([
                 { block: 'block' },
                 { block: 'block', modName: 'mod', modVal: 'val-1' },
                 { block: 'block', modName: 'mod', modVal: 'val-2' }
@@ -73,14 +113,40 @@ describe('normalize --harmony', function () {
 
     describe('elems', function () {
         it('must support elem by BEM-notation', function () {
-            normalize({ block: 'block', elem: 'elem' }).must.eql([
+            var entities = {
+                block: 'block',
+                elem: 'elem'
+            };
+
+            normalize(entities).must.eql([
                 { block: 'block' },
-                { block: 'block',  elem: 'elem' }
+                { block: 'block', elem: 'elem' }
+            ]);
+        });
+
+        it('must support shortcat for boolean mod of elem by BEM-notation', function () {
+            var entities = {
+                block: 'block',
+                elem: 'elem',
+                modName: 'mod'
+            };
+
+            normalize(entities).must.eql([
+                { block: 'block' },
+                { block: 'block', elem: 'elem' },
+                { block: 'block', elem: 'elem', modName: 'mod', modVal: true }
             ]);
         });
 
         it('must support boolean mod of elem by BEM-notation', function () {
-            normalize({ block: 'block', elem: 'elem', modName: 'mod', modVal: true }).must.eql([
+            var entities = {
+                block: 'block',
+                elem: 'elem',
+                modName: 'mod',
+                modVal: true
+            };
+
+            normalize(entities).must.eql([
                 { block: 'block' },
                 { block: 'block', elem: 'elem' },
                 { block: 'block', elem: 'elem', modName: 'mod', modVal: true }
@@ -88,7 +154,14 @@ describe('normalize --harmony', function () {
         });
 
         it('must support elem mod by BEM-notation', function () {
-            normalize({ block: 'block', elem: 'elem', modName: 'mod', modVal: 'val' }).must.eql([
+            var entities = {
+                block: 'block',
+                elem: 'elem',
+                modName: 'mod',
+                modVal: 'val'
+            };
+
+            normalize(entities).must.eql([
                 { block: 'block' },
                 { block: 'block', elem: 'elem' },
                 { block: 'block', elem: 'elem', modName: 'mod', modVal: 'val' }
@@ -96,7 +169,12 @@ describe('normalize --harmony', function () {
         });
 
         it('must support elems as arrays', function () {
-            normalize({ block: 'block', elems: ['elem-1', 'elem-2'] }).must.eql([
+            var entities = {
+                block: 'block',
+                elems: ['elem-1', 'elem-2']
+            };
+
+            normalize(entities).must.eql([
                 { block: 'block' },
                 { block: 'block', elem: 'elem-1' },
                 { block: 'block',  elem: 'elem-2' }
@@ -104,7 +182,13 @@ describe('normalize --harmony', function () {
         });
 
         it('must support elem mods as object', function () {
-            normalize({ block: 'block', elem: 'elem', mods: { mod: 'val' } }).must.eql([
+            var entities = {
+                block: 'block',
+                elem: 'elem',
+                mods: { mod: 'val' }
+            };
+
+            normalize(entities).must.eql([
                 { block: 'block' },
                 { block: 'block', elem: 'elem' },
                 { block: 'block', elem: 'elem', modName: 'mod', modVal: 'val' }
@@ -112,7 +196,13 @@ describe('normalize --harmony', function () {
         });
 
         it('must support boolean mods of elem as array', function () {
-            normalize({ block: 'block', elem: 'elem', mods: ['mod-1', 'mod-2'] }).must.eql([
+            var entities = {
+                block: 'block',
+                elem: 'elem',
+                mods: ['mod-1', 'mod-2']
+            };
+
+            normalize(entities).must.eql([
                 { block: 'block' },
                 { block: 'block', elem: 'elem' },
                 { block: 'block', elem: 'elem', modName: 'mod-1', modVal: true },
@@ -121,32 +211,17 @@ describe('normalize --harmony', function () {
         });
 
         it('must support mod values of elem as array', function () {
-            normalize({ block: 'block', elem: 'elem', mods: { mod: ['val-1', 'val-2'] } }).must.eql([
+            var entities = {
+                block: 'block',
+                elem: 'elem',
+                mods: { mod: ['val-1', 'val-2'] }
+            };
+
+            normalize(entities).must.eql([
                 { block: 'block' },
                 { block: 'block', elem: 'elem' },
                 { block: 'block', elem: 'elem', modName: 'mod', modVal: 'val-1' },
                 { block: 'block', elem: 'elem', modName: 'mod', modVal: 'val-2' }
-            ]);
-        });
-    });
-
-    describe('mix', function () {
-        it('must support elems with block mods', function () {
-            normalize({ block: 'block', elems: ['elem-1', 'elem-2'], mods: ['mod-1', 'mod-2'] }).must.eql([
-                { block: 'block' },
-                { block: 'block', elem: 'elem-1' },
-                { block: 'block', elem: 'elem-2' },
-                { block: 'block', modName: 'mod-1', modVal: true },
-                { block: 'block', modName: 'mod-2', modVal: true }
-            ]);
-        });
-
-        it('must support elems with block mod', function () {
-            normalize({ block: 'block', elems: ['elem-1', 'elem-2'], modName: 'mod' }).must.eql([
-                { block: 'block' },
-                { block: 'block', elem: 'elem-1' },
-                { block: 'block', elem: 'elem-2' },
-                { block: 'block', modName: 'mod', modVal: true }
             ]);
         });
     });
--- test/normalize.test.js
+++ test/normalize.test.js
@@ -10,32 +10,56 @@ describe('normalize', function () {
     });
 
     it('must support objects', function () {
-        normalize({ name: 'block' }).must.eql([
+        var entities = { name: 'block' };
+
+        normalize(entities).must.eql([
             { block: 'block' }
         ]);
     });
 
     it('must support array', function () {
-        normalize([{ name: 'A' }, { name: 'B' }]).must.eql([
-            { block: 'A' }, { block: 'B' }
+        var entities = [
+            { name: 'block-1' },
+            { name: 'block-2' }
+        ];
+
+        normalize(entities).must.eql([
+            { block: 'block-1' },
+            { block: 'block-2' }
         ]);
     });
 
     describe('mods', function () {
         it('must support objects', function () {
-            normalize({ name: 'block', mods: [{ name: 'mod', vals: [{ name: 'val' }] }] }).must.eql([
+            var entities = { name: 'block', mods: [{ name: 'mod', vals: [{ name: 'val' }] }] };
+
+            normalize(entities).must.eql([
                 { block: 'block' },
                 { block: 'block', modName: 'mod', modVal: 'val' }
+            ]);
+        });
+
+        it('must support shortcat for boolean mod as objects', function () {
+            var entities = { name: 'block', mods: [{ name: 'mod' }] };
+
+            normalize(entities).must.eql([
+                { block: 'block' },
+                { block: 'block', modName: 'mod', modVal: true }
             ]);
         });
     });
 
     describe('elems', function () {
         it('must support arrays', function () {
-            normalize({
+            var entities = {
                 name: 'block',
-                elems: [{ name: 'elem-1' }, { name: 'elem-2' }]
-            }).must.eql([
+                elems: [
+                    { name: 'elem-1' },
+                    { name: 'elem-2' }
+                ]
+            };
+
+            normalize(entities).must.eql([
                 { block: 'block' },
                 { block: 'block', elem: 'elem-1' },
                 { block: 'block', elem: 'elem-2' }
@@ -43,13 +67,32 @@ describe('normalize', function () {
         });
 
         it('must support objects', function () {
-            normalize({
+            var entities = {
                 name: 'block',
-                elems: [{ name: 'elem', mods: [{ name: 'mod', vals: [{ name: 'val' }] }] }]
-            }).must.eql([
+                elems: [
+                    { name: 'elem', mods: [{ name: 'mod', vals: [{ name: 'val' }] }] }
+                ]
+            };
+
+            normalize(entities).must.eql([
                 { block: 'block' },
                 { block: 'block', elem: 'elem' },
                 { block: 'block', elem: 'elem', modName: 'mod', modVal: 'val' }
+            ]);
+        });
+
+        it('must support shortcat for boolean mod as objects', function () {
+            var entities = {
+                name: 'block',
+                elems: [
+                    { name: 'elem', mods: [{ name: 'mod' }] }
+                ]
+            };
+
+            normalize(entities).must.eql([
+                { block: 'block' },
+                { block: 'block', elem: 'elem' },
+                { block: 'block', elem: 'elem', modName: 'mod', modVal: true }
             ]);
         });
     });
