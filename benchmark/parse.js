6a8ab8f104b939a335b294ad4eea1e55348b3da6
andrewblond <andrewblond@yandex.com>
2015-01-12 02:45:28 +0300

    Added benchmarks

---
+++ benchmark/parse.js
@@ -0,0 +1,27 @@
+var naming = require('../lib/bem-naming'),
+    strings = {
+        block: 'block',
+        blockMod: 'block_mod-name_mod-val',
+        elem: 'block__elem',
+        elemMod: 'block__elem_mod-name_mod-val'
+    };
+
+suite('parse', function () {
+    set('iterations', 2000000);
+
+    bench('block', function () {
+        naming.parse(strings.block);
+    });
+
+    bench('blockMod', function () {
+        naming.parse(strings.blockMod);
+    });
+
+    bench('elem', function () {
+        naming.parse(strings.elem);
+    });
+
+    bench('elemMod', function () {
+        naming.parse(strings.elemMod);
+    });
+});
---
+++ benchmark/stringify.js
@@ -0,0 +1,27 @@
+var naming = require('../lib/bem-naming'),
+    notations = {
+        block: { block: 'block' },
+        blockMod: { block: 'block', modName: 'mod-name', modVal: 'mod-val' },
+        elem: { block: 'block', elem: 'elem' },
+        elemMod: { block: 'block', elem: 'elem', modName: 'mod-name', modVal: 'mod-val' }
+    };
+
+suite('stringify', function () {
+    set('iterations', 2000000);
+
+    bench('block', function () {
+        naming.stringify(notations.block);
+    });
+
+    bench('blockMod', function () {
+        naming.stringify(notations.blockMod);
+    });
+
+    bench('elem', function () {
+        naming.stringify(notations.elem);
+    });
+
+    bench('elemMod', function () {
+        naming.stringify(notations.elemMod);
+    });
+});
