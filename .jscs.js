93b3c8ebbac27968635b2fe30b8e748b782aad24
andrewblond <andrewblond@yandex.com>
2015-03-27 15:20:41 +0300

    Fixed ignore configs: should ignore only libs for benchmarks

--- .jscs.js
+++ .jscs.js
@@ -2,7 +2,7 @@ module.exports = {
     excludeFiles: [
         'node_modules/**',
         'coverage/**',
-        'benchmark/fixtures/**',
+        'benchmark/fixtures/libs/**',
         'benchmark/node_modules/**'
     ],
     requireSpaceAfterKeywords: ['if', 'else', 'for', 'while', 'do', 'switch', 'return', 'try', 'catch'],
