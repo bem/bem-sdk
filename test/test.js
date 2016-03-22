8ead9bc8d811c09d1e4fface3c9b163fb5944ba9
Vladimir Grinenko <i@tadatuta.com>
2016-03-22 15:54:12 +0300

    Implement flat scheme

--- test/test.js
+++ test/test.js
@@ -59,5 +59,59 @@ describe('default', function() {
                 }, 'js')
             ).eql('a/__e1/_mn/a__e1_mn_mv.js');
         });
+
+        it('should support optional naming style', function() {
+            expect(
+                scheme('nested').path({
+                    block: 'a',
+                    elem: 'e1',
+                    modName: 'mn',
+                    modVal: 'mv'
+                }, 'js', { naming: { elem: '%%%', mod: '###' }})
+            ).eql('a/%%%e1/###mn/a%%%e1###mn###mv.js');
+        });
+    });
+
+    describe('lib/schemes/flat', function() {
+        it('should return path for a block', function() {
+            expect(scheme('flat').path({ block: 'a' }, 'js'))
+                .eql('a.js');
+        });
+
+        it('should return path for a block with modifier', function() {
+            expect(
+                scheme('flat').path({
+                    block: 'a', modName: 'mn', modVal: 'mv'
+                }, 'js')
+            ).eql('a_mn_mv.js');
+        });
+
+        it('should return path for elem', function() {
+            expect(
+                scheme('flat').path({ block: 'a', elem: 'e1' }, 'js')
+            ).eql('a__e1.js');
+        });
+
+        it('should return path for modName elem', function() {
+            expect(
+                scheme('flat').path({
+                    block: 'a',
+                    elem: 'e1',
+                    modName: 'mn',
+                    modVal: 'mv'
+                }, 'js')
+            ).eql('a__e1_mn_mv.js');
+        });
+
+        it('should support optional naming style', function() {
+            expect(
+                scheme('flat').path({
+                    block: 'a',
+                    elem: 'e1',
+                    modName: 'mn',
+                    modVal: 'mv'
+                }, 'js', { naming: { elem: '%%%', mod: '###' }})
+            ).eql('a%%%e1###mn###mv.js');
+        });
     });
 });
