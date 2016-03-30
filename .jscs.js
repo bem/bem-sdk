4c78d4f8ea090526488284ceef2e99f0dafa2457
blond <andrewblond@yandex.com>
2016-03-30 14:18:25 +0300

    lint: fix jscs config

--- .jscs.js
+++ .jscs.js
@@ -48,11 +48,6 @@ module.exports = {
     requireCapitalizedConstructors: true,
     safeContextKeyword: ['_this'],
     disallowYodaConditions: true,
-    validateJSDoc: {
-        checkParamNames: true,
-        checkRedundantParams: true,
-        requireParamTypes: true
-    },
     requireSpaceAfterLineComment: true,
     disallowNewlineBeforeBlockStatements: true
 };
