7179f50a0a057499787878045a951c9356006b15
tormozz48 <tormozz48@gmail.com>
2016-04-03 14:58:56 +0300

    Provide refactoring with help of lodash library. Add JSDocs

--- index.js
+++ index.js
@@ -1,59 +1,85 @@
 'use strict';
 
 const path = require('path');
+const _ = require('lodash');
 const rc = require('./lib/rc');
-const mergeWith = require('lodash.mergewith');
 const glob = require('glob');
 
+function globPromise(pattern, options) {
+    return new Promise((resolve, reject) => {
+        glob(pattern, options, (err, results) => {
+            err ? reject(err) : resolve(results);
+        });
+    });
+}
+
+/**
+ * Constructor
+ * @param {Object} [options] object
+ * @param {String} [options.name='bem'] - config filename.
+ * @param {String} [options.projectRoot=process.cwd()] project root directory.
+ * @param {Object} [options.config={}] extends found configs with this object
+ * @param {String} [options.argv] custom path to config on FS via command line argument `--config`
+ * @constructor
+ */
 function BemConfig(options) {
     this._options = options || {};
     this._options.projectRoot || (this._options.projectRoot = process.cwd());
 }
 
+/**
+ * Returns all found configs
+ * @returns {Promise}
+ */
 BemConfig.prototype.configs = function() {
     const options = this._options;
     const projectRoot = options.projectRoot;
 
-    return Promise.all(rc({
+    const readConfigs = rc({
         name: options.name || 'bem',
         defaults: options.config,
         argv: options.argv,
         projectRoot
-    })
-    .map(function(config) {
-        if (!config.levels) { return config; }
+    });
 
-        return Promise.all(Object.keys(config.levels).map(wildcardLevel => {
-            return new Promise((resolve, reject) => {
-                // replace wildcard levels with resolved onces
-                // in the same config object by reference
+    function createProcessConfigFunc(config, wildcardLevel) {
+        return resolvedWildcards => {
+            resolvedWildcards.forEach((level, idx) => {
+                if (wildcardLevel === path.resolve(level)) { return; }
 
-                glob(wildcardLevel, { cwd: projectRoot }, (err, resolvedWildcards) => {
-                    if (err) { return reject(err); }
+                config.levels[path.resolve(projectRoot, level)] = config.levels[wildcardLevel];
 
-                    resolvedWildcards.forEach((level, idx) => {
-                        if (wildcardLevel === path.resolve(level)) { return; }
-
-                        config.levels[path.resolve(projectRoot, level)] = config.levels[wildcardLevel];
-
-                        if (resolvedWildcards.length === idx + 1) {
-                            delete config.levels[wildcardLevel];
-                        }
-                    });
-
-                    resolve(config);
-                });
+                if (resolvedWildcards.length === idx + 1) {
+                    delete config.levels[wildcardLevel];
+                }
             });
-        })).then(() => config);
+            return config;
+        };
+    }
+
+    return Promise.all(readConfigs.map(config => {
+        return !config.levels ? config : Promise.all(Object.keys(config.levels).map(wildcardLevel => {
+            return globPromise(wildcardLevel, { cwd: projectRoot })
+                .then(createProcessConfigFunc(config, wildcardLevel));
+        }))
+        .then(() => config);
     }));
 };
 
-// merged config
+/**
+ * Returns merged config
+ * @returns {Promise}
+ */
 BemConfig.prototype.get = function() {
     return this.configs()
         .then(all => Object.assign.apply(Object, [{}].concat(all)));
 };
 
+/**
+ * Resolves config for given level
+ * @param {String} levelPath - level path
+ * @returns {Promise}
+ */
 BemConfig.prototype.level = function(levelPath) {
     const absLevelPath = path.resolve(this._options.projectRoot, levelPath);
     let levelOpts = { __source: absLevelPath };
@@ -69,7 +95,7 @@ BemConfig.prototype.level = function(levelPath) {
                 if (level === absLevelPath) {
 
                     // works like deep extend but overrides arrays
-                    levelOpts = mergeWith({}, levels[level], levelOpts, (objValue, srcValue) => {
+                    levelOpts = _.mergeWith({}, levels[level], levelOpts, (objValue, srcValue) => {
                         if (Array.isArray(objValue)) {
                             return srcValue;
                         }
@@ -84,18 +110,24 @@ BemConfig.prototype.level = function(levelPath) {
         delete levelOpts.levels;
         delete levelOpts.root;
 
-        if (!Object.keys(levelOpts).length) { return; }
-
-        return levelOpts;
+        return !_.isEmpty(levelOpts) ? levelOpts : undefined;
     });
 };
 
+/**
+ * Returns config for given library
+ * @param {String} libName - library name
+ * @returns {Promise}
+ */
 BemConfig.prototype.library = function(libName) {
-    return this.get().then(config => {
-        return new BemConfig({ projectRoot: config.libs[libName].path });
-    });
+    return this.get()
+        .then(config => new BemConfig({ projectRoot: config.libs[libName].path }));
 };
 
+/**
+ * Returns map of settings for each of level
+ * @returns {Promise}
+ */
 BemConfig.prototype.levelMap = function() {
     return this.get().then(config => {
         const projectLevels = config.levels;
@@ -106,25 +138,22 @@ BemConfig.prototype.levelMap = function() {
                 .then(libConfig => libConfig.levels);
         }))
         .then(libLevels => libLevels.concat(projectLevels))
-        .then(allLevels => {
-            return allLevels.reduce((prev, levels) => {
-                Object.keys(levels).forEach(level => {
-                    prev[level] = levels[level];
-                });
-
-                return prev;
-            }, {});
-        });
+        .then(allLevels => _.merge.apply(this, allLevels));
 
     });
 };
 
+/**
+ * Returns config for given module name
+ * @param {String} moduleName - name of module
+ * @returns {Promise}
+ */
 BemConfig.prototype.module = function(moduleName) {
     return this.get().then(config => {
         const modules = config.modules;
 
         return modules && modules[moduleName];
     });
-}
+};
 
 module.exports = options => new BemConfig(options);
