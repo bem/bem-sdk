ac00d7079e64cddd85bc589921b1345f852219b6
blond <andrewblond@yandex.com>
2016-08-13 17:28:59 +0300

    test(typeOf): remove `typeOf` method

--- test/presets/two-dashes/is-elem.test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const test = require('ava');
-const naming = require('../../../index')('two-dashes');
-const isElem = naming.isElem;
-
-test('should detect elem', t => {
-    t.true(isElem('block__elem'));
-});
