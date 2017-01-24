1a4f7e3f01ca87d9f248943a796ed9c2655b3840
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2017-01-24 08:07:23 +0300

    Add support for @bem/cell

--- test/test.js
+++ test/test.js
@@ -1,16 +1,34 @@
 var expect = require('chai').expect,
+    BemCell = require('@bem/cell'),
+    BemEntityName = require('@bem/entity-name'),
     scheme = require('..');
 
 describe('default', function() {
 
     it('should return path + tech', function() {
         expect('a/a.js')
-            .eql(scheme().path({ block: 'a' }, 'js'));
+            .eql(scheme().path({block: 'a'}, 'js'));
+
+        expect('a/a.js')
+            .eql(scheme().path(
+                new BemCell({
+                    entity: new BemEntityName({block: 'a'}),
+                    tech: 'js'
+                })),
+                'bemCell - api'
+            );
     });
 
     it('should return nested scheme by default', function() {
-        expect(scheme().path({ block: 'a', elem: 'e1' }, 'js'))
+        expect(scheme().path({block: 'a', elem: 'e1'}, 'js'))
             .eql('a/__e1/a__e1.js');
+
+        expect(scheme().path(
+                new BemCell({
+                    entity: new BemEntityName({block: 'a', elem: 'e1'}),
+                    tech: 'js'
+                }))
+        ).eql('a/__e1/a__e1.js', 'bemCell - api');
     });
 
     it('should return error', function() {
@@ -25,14 +43,34 @@ describe('default', function() {
                 elem: 'e1',
                 modName: 'mn',
                 modVal: 'mv'
-            }, 'js', { naming: { elem: '%%%', mod: '###' }})
+            }, 'js', {naming: {elem: '%%%', mod: '###'}})
         ).eql('a/%%%e1/###mn/a%%%e1###mn###mv.js');
+
+        expect(
+            scheme('nested').path(
+                new BemCell({
+                    entity: new BemEntityName({
+                        block: 'a',
+                        elem: 'e1',
+                        mod: {name: 'mn', val: 'mv'}
+                    }),
+                    tech: 'js'
+                }),
+                {naming: {elem: '%%%', mod: '###'}})
+        ).eql('a/%%%e1/###mn/a%%%e1###mn###mv.js', 'bemCell - api');
     });
 
     describe('lib/schemes/nested', function() {
         it('should return path for a block', function() {
-            expect(scheme('nested').path({ block: 'a' }, 'js'))
+            expect(scheme('nested').path({block: 'a'}, 'js'))
                 .eql('a/a.js');
+
+            expect(scheme('nested').path(
+                new BemCell({
+                    entity: new BemEntityName({block: 'a'}),
+                    tech: 'js'
+                })
+            )).eql('a/a.js', 'bemCell - api');
         });
 
         it('should return path for a block with modifier', function() {
@@ -41,6 +79,12 @@ describe('default', function() {
                     block: 'a', modName: 'mn', modVal: 'mv'
                 }, 'js')
             ).eql('a/_mn/a_mn_mv.js');
+
+            expect(
+                scheme('nested').path({
+                    block: 'a', modName: 'mn', modVal: 'mv'
+                }, 'js')
+            ).eql('a/_mn/a_mn_mv.js', 'bemCell - api');
         });
 
         it('should return path for a block with boolean modifier', function() {
@@ -49,6 +93,14 @@ describe('default', function() {
                     block: 'a', modName: 'mn', modVal: true
                 }, 'js')
             ).eql('a/_mn/a_mn.js');
+
+            expect(
+                scheme('nested').path(
+                new BemCell({
+                    entity: new BemEntityName({block: 'a', mod: {name: 'mn', val: true }}),
+                    tech: 'js'
+                }))
+            ).eql('a/_mn/a_mn.js', 'bemCell - api');
         });
 
         it('should return path for a block with modifier without value', function() {
@@ -57,12 +109,30 @@ describe('default', function() {
                     block: 'a', modName: 'mn'
                 }, 'js')
             ).eql('a/_mn/a_mn.js');
+
+            expect(
+                scheme('nested').path(
+                    new BemCell({
+                        entity: new BemEntityName({block: 'a', mod: {name: 'mn'}}),
+                        tech: 'js'
+                    })
+                )
+            ).eql('a/_mn/a_mn.js', 'bemCell - api');
         });
 
         it('should return path for elem', function() {
             expect(
-                scheme('nested').path({ block: 'a', elem: 'e1' }, 'js')
+                scheme('nested').path({block: 'a', elem: 'e1'}, 'js')
             ).eql('a/__e1/a__e1.js');
+
+            expect(
+                scheme('nested').path(
+                    new BemCell({
+                        entity: new BemEntityName({block: 'a', elem: 'e1'}),
+                        tech: 'js'
+                    })
+                )
+            ).eql('a/__e1/a__e1.js', 'bemCell - api');
         });
 
         it('should return path for modName elem', function() {
@@ -74,6 +144,19 @@ describe('default', function() {
                     modVal: 'mv'
                 }, 'js')
             ).eql('a/__e1/_mn/a__e1_mn_mv.js');
+
+            expect(
+                scheme('nested').path(
+                    new BemCell({
+                        entity: new BemEntityName({
+                            block: 'a',
+                            elem: 'e1',
+                            mod: {name: 'mn', val: 'mv'}
+                        }),
+                        tech: 'js'
+                    })
+                )
+            ).eql('a/__e1/_mn/a__e1_mn_mv.js', 'bemCell - api');
         });
 
         it('should support optional naming style', function() {
@@ -83,15 +166,52 @@ describe('default', function() {
                     elem: 'e1',
                     modName: 'mn',
                     modVal: 'mv'
-                }, 'js', { naming: { elem: '%%%', mod: '###' }})
+                }, 'js', {naming: {elem: '%%%', mod: '###'}})
             ).eql('a/%%%e1/###mn/a%%%e1###mn###mv.js');
+
+            expect(
+                scheme('nested').path(
+                    new BemCell({
+                        entity: new BemEntityName({
+                            block: 'a',
+                            elem: 'e1',
+                            mod: {name: 'mn', val: 'mv'}
+                        }),
+                        tech: 'js'
+                    }),
+                    {naming: {elem: '%%%', mod: '###'}}
+                )
+            ).eql('a/%%%e1/###mn/a%%%e1###mn###mv.js', 'bemCell - api');
+        });
+
+        it('should support layer for BemCell', function() {
+            expect(
+                scheme('nested').path(
+                    new BemCell({
+                        entity: new BemEntityName({
+                            block: 'a',
+                            elem: 'e1',
+                            mod: {name: 'mn', val: 'mv'}
+                        }),
+                        tech: 'js',
+                        layer: 'common.blocks'
+                    })
+                )
+            ).eql('common.blocks/a/__e1/_mn/a__e1_mn_mv.js', 'bemCell - api');
         });
     });
 
     describe('lib/schemes/flat', function() {
         it('should return path for a block', function() {
-            expect(scheme('flat').path({ block: 'a' }, 'js'))
+            expect(scheme('flat').path({block: 'a'}, 'js'))
                 .eql('a.js');
+
+            expect(scheme('flat').path(
+                new BemCell({
+                    entity: new BemEntityName({block: 'a'}),
+                    tech: 'js'
+                })
+            )).eql('a.js', 'bemCell - api');
         });
 
         it('should return path for a block with modifier', function() {
@@ -100,12 +220,28 @@ describe('default', function() {
                     block: 'a', modName: 'mn', modVal: 'mv'
                 }, 'js')
             ).eql('a_mn_mv.js');
+
+            expect(scheme('flat').path(
+                new BemCell({
+                    entity: new BemEntityName({block: 'a', mod: {name: 'mn', val: 'mv'}}),
+                    tech: 'js'
+                })
+            )).eql('a_mn_mv.js', 'bemCell - api');
         });
 
         it('should return path for elem', function() {
             expect(
-                scheme('flat').path({ block: 'a', elem: 'e1' }, 'js')
+                scheme('flat').path({block: 'a', elem: 'e1'}, 'js')
             ).eql('a__e1.js');
+
+            expect(
+                scheme('flat').path(
+                    new BemCell({
+                        entity: new BemEntityName({block: 'a', elem: 'e1'}),
+                        tech: 'js'
+                    })
+                )
+            ).eql('a__e1.js', 'bemCell - api');
         });
 
         it('should return path for modName elem', function() {
@@ -117,6 +253,19 @@ describe('default', function() {
                     modVal: 'mv'
                 }, 'js')
             ).eql('a__e1_mn_mv.js');
+
+            expect(
+                scheme('flat').path(
+                    new BemCell({
+                        entity: new BemEntityName({
+                            block: 'a',
+                            elem: 'e1',
+                            mod: {name: 'mn', val: 'mv'}
+                        }),
+                        tech: 'js'
+                    })
+                )
+            ).eql('a__e1_mn_mv.js', 'bemCell - api');
         });
 
         it('should support optional naming style', function() {
@@ -126,8 +275,38 @@ describe('default', function() {
                     elem: 'e1',
                     modName: 'mn',
                     modVal: 'mv'
-                }, 'js', { naming: { elem: '%%%', mod: '###' }})
+                }, 'js', {naming: {elem: '%%%', mod: '###'}})
             ).eql('a%%%e1###mn###mv.js');
+
+            expect(
+                scheme('flat').path(
+                    new BemCell({
+                        entity: new BemEntityName({
+                            block: 'a',
+                            elem: 'e1',
+                            mod: {name: 'mn', val: 'mv'}
+                        }),
+                        tech: 'js'
+                    }),
+                    {naming: {elem: '%%%', mod: '###'}}
+                )
+            ).eql('a%%%e1###mn###mv.js', 'bemCell - api');
+        });
+
+        it('should support layer for BemCell', function() {
+            expect(
+                scheme('flat').path(
+                    new BemCell({
+                        entity: new BemEntityName({
+                            block: 'a',
+                            elem: 'e1',
+                            mod: {name: 'mn', val: 'mv'}
+                        }),
+                        tech: 'js',
+                        layer: 'common.blocks'
+                    })
+                )
+            ).eql('common.blocks/a__e1_mn_mv.js', 'bemCell - api');
         });
     });
 });
