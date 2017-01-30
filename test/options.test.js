9c8cfd180405969a5c4c4978c2c764bae614ca6c
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2017-01-30 20:52:09 +0300

    Breaking: Change construct API to suit presets.

--- test/options.test.js
+++ test/options.test.js
@@ -13,27 +13,27 @@ test('should throw error if specified preset is unknow', t => {
 });
 
 test('should provide elem option', t => {
-    const myNaming = naming({ elem: '==' });
+    const myNaming = naming({ delims: { elem: '==' } });
 
     t.is(myNaming.elemDelim, '==');
 });
 
 test('should support mod option as string', t => {
-    const myNaming = naming({ mod: '--' });
+    const myNaming = naming({ delims: { mod: '--' } });
 
     t.is(myNaming.modDelim, '--');
     t.is(myNaming.modValDelim, '--');
 });
 
 test('should support mod option as object', t => {
-    const myNaming = naming({ mod: { name: '--', val: '_' } });
+    const myNaming = naming({ delims: { mod: { name: '--', val: '_' } } });
 
     t.is(myNaming.modDelim, '--');
     t.is(myNaming.modValDelim, '_');
 });
 
 test('should use modDelim if mod.val is not specified', t => {
-    const myNaming = naming({ mod: { name: '--' } });
+    const myNaming = naming({ delims: { mod: { name: '--' } } });
 
     t.is(myNaming.modDelim, '--');
     t.is(myNaming.modValDelim, '--');
