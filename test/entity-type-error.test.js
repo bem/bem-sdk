07b46b1d037fbb14c6d6268a67f43e53b9bbdb1c
blond <andrewblond@yandex.com>
2017-04-15 12:52:49 +0300

    test(EntityTypeError): add tests for EntityTypeError

---
+++ test/entity-type-error.test.js
@@ -0,0 +1,45 @@
+const test = require('ava');
+
+const EntityTypeError = require('../lib/entity-type-error');
+
+test('should create type error', t => {
+    const error = new EntityTypeError();
+
+    t.is(error.message, 'the  `undefined` is not valid BEM entity');
+});
+
+test('should create type error with number', t => {
+    const error = new EntityTypeError(42);
+
+    t.is(error.message, 'the number `42` is not valid BEM entity');
+});
+
+test('should create type error with string', t => {
+    const error = new EntityTypeError('block');
+
+    t.is(error.message, 'the string `\'block\'` is not valid BEM entity');
+});
+
+test('should create type error with empty object', t => {
+    const error = new EntityTypeError({});
+
+    t.is(error.message, 'the object `{}` is not valid BEM entity');
+});
+
+test('should create type error with object', t => {
+    const error = new EntityTypeError({ key: 'val' });
+
+    t.is(error.message, "the object `{ key: 'val' }` is not valid BEM entity");
+});
+
+test('should create type error with deep object', t => {
+    const error = new EntityTypeError({ a: { b: { c: 'd' } } });
+
+    t.is(error.message, 'the object `{ a: { b: [Object] } }` is not valid BEM entity');
+});
+
+test('should create type error with reason', t => {
+    const error = new EntityTypeError({ elem: 'elem' }, 'the field `block` is undefined');
+
+    t.is(error.message, "the object `{ elem: 'elem' }` is not valid BEM entity, the field `block` is undefined");
+});
