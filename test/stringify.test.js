c14d80703c7abbf8cd90536d536f46e9b448bf9d
Alexey Yaroshevich <zxqfox@gmail.com>
2017-05-22 07:03:44 +0300

    fix: stringify should return string in any way

--- test/stringify.test.js
+++ test/stringify.test.js
@@ -8,7 +8,7 @@ const naming = require('../index');
 test('should not stringify not valid notation', t => {
     const str = naming.stringify({});
 
-    t.is(str, undefined);
+    t.is(str, '');
 });
 
 test('should support block instance of BemEntityName', t => {
