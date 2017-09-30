7fe09e2615f49c8842b16562ef027524a0e80c50
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2017-09-30 15:21:33 +0300

    test: Fix cell tests

--- packages/cell/test/inspect.test.js
+++ packages/cell/test/inspect.test.js
@@ -1,40 +1,22 @@
 'use strict';
 
+const util = require('util');
+
 const describe = require('mocha').describe;
 const it = require('mocha').it;
-const beforeEach = require('mocha').beforeEach;
-const afterEach = require('mocha').afterEach;
-
 const expect = require('chai').expect;
-const sinon = require('sinon');
 
 const BemEntityName = require('@bem/sdk.entity-name');
-
 const BemCell = require('../index');
 
-const EOL = require('os').EOL;
-
 describe('inspect', () => {
-    let stdoutWriteStub;
-
-    beforeEach(() => {
-        stdoutWriteStub = sinon.stub(process.stdout, 'write');
-    });
-
-    afterEach(() => {
-        stdoutWriteStub.restore();
-    });
-
     it('should return entity object', () => {
         const cell = new BemCell({
             entity: new BemEntityName({ block: 'block' }),
             tech: 'css'
         });
 
-        console.log(cell);
-
-        const message = `BemCell { entity: { block: 'block' }, tech: 'css' }${EOL}`;
-
-        expect(stdoutWriteStub.calledWith(message)).to.equal(true);
+        const message = `BemCell { entity: { block: 'block' }, tech: 'css' }`;
+        expect(util.inspect(cell)).to.equal(message);
     });
 });
