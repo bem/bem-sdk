184a6c3a5f7ed7f3df6f7aea1072249dbc1377a2
Andrew Abramov <blond@yandex-team.ru>
2017-10-02 17:05:07 +0300

    test(naming.entity.stringify): use mocha

--- packages/naming.entity.stringify/test/stringify.test.js
+++ packages/naming.entity.stringify/test/stringify.test.js
@@ -1,41 +1,43 @@
 'use strict';
 
-const test = require('ava');
-const BemEntityName = require('@bem/entity-name');
+const expect = require('chai').expect;
+const BemEntityName = require('@bem/sdk.entity-name');
 
 const originNaming = require('@bem/sdk.naming.presets/origin');
 const stringify = require('..')(originNaming);
 
-test('should not stringify not valid notation', t => {
-    const str = stringify({});
+describe('naming.entity.stringify', () => {
+    it('should not stringify not valid notation', () => {
+        const str = stringify({});
 
-    t.is(str, '');
-});
+        expect(str).to.eql('');
+    });
 
-test('should support block instance of BemEntityName', t => {
-    const obj = { block: 'block' };
-    const entityName = new BemEntityName(obj);
+    it('should support block instance of BemEntityName', () => {
+        const obj = { block: 'block' };
+        const entityName = new BemEntityName(obj);
 
-    t.is(stringify(obj), stringify(entityName));
-});
+        expect(stringify(obj)).to.eql(stringify(entityName));
+    });
 
-test('should support modifier instance of BemEntityName', t => {
-    const obj = { block: 'block', mod: 'mod' };
-    const entityName = new BemEntityName(obj);
+    it('should support modifier instance of BemEntityName', () => {
+        const obj = { block: 'block', mod: 'mod' };
+        const entityName = new BemEntityName(obj);
 
-    t.is(stringify(obj), stringify(entityName));
-});
+        expect(stringify(obj)).to.eql(stringify(entityName));
+    });
 
-test('should support element instance of BemEntityName', t => {
-    const obj = { block: 'block', elem: 'elem' };
-    const entityName = new BemEntityName(obj);
+    it('should support element instance of BemEntityName', () => {
+        const obj = { block: 'block', elem: 'elem' };
+        const entityName = new BemEntityName(obj);
 
-    t.is(stringify(obj), stringify(entityName));
-});
+        expect(stringify(obj)).to.eql(stringify(entityName));
+    });
 
-test('should support element modifier instance of BemEntityName', t => {
-    const obj = { block: 'block', mod: 'mod' };
-    const entityName = new BemEntityName(obj);
+    it('should support element modifier instance of BemEntityName', () => {
+        const obj = { block: 'block', mod: 'mod' };
+        const entityName = new BemEntityName(obj);
 
-    t.is(stringify(obj), stringify(entityName));
+        expect(stringify(obj)).to.eql(stringify(entityName));
+    });
 });
