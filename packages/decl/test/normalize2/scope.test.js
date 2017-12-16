c97c5e7567ddd64034c4047bb58145d6042ee6a5
andrewblond <andrewblond@yandex.com>
2017-12-16 20:18:43 +0300

    fix(decl/normalize/v2): should consider scope for object with tech field

---
+++ packages/decl/test/normalize2/scope.test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+
+const expect = require('chai').expect;
+
+const simplifyCell = require('../util').simplifyCell;
+const normalize = require('../../lib/normalize/v2');
+
+describe('normalize2.scope', () => {
+    it('should consider block scope', () => {
+        const decl = {};
+        const scope = simplifyCell({ entity: { block: 'block' } });
+
+        expect(normalize(decl, scope).map(simplifyCell)).to.deep.equal([
+            { entity: { block: 'block' }, tech: null }
+        ]);
+    });
+
+    it('should consider scope for object with tech field', () => {
+        const decl = { tech: 'js' };
+        const scope = simplifyCell({ entity: { block: 'block' } });
+
+        expect(normalize(decl, scope).map(simplifyCell)).to.deep.equal([
+            { entity: { block: 'block' }, tech: 'js' }
+        ]);
+    });
+});
