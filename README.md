8aafb7684e4f1eb3a2dbfea0abb269d84160b8c6
Alexey Yaroshevich <zxqfox@gmail.com>
2018-06-23 09:47:34 +0300

    docs: simplify readme

--- README.md
+++ README.md
@@ -4,39 +4,40 @@
     <img width="300" height="300" src="https://rawgithub.com/bem/bem-sdk/master/logo.svg" alt="logo" />
 </div>
 
-Usefull modules to work with [BEM][] methodology.
+Useful modules to work with projects based on principles of [BEM][] methodology.
 
 ## General
 
-* [@bem/sdk.walk](https://github.com/bem/bem-sdk/tree/master/packages/walk) — traversing a BEM project's file system
-* [@bem/sdk.config](https://github.com/bem/bem-sdk/tree/master/packages/config) — keeping all configuration of BEM project in one place
+* [walk](https://github.com/bem/bem-sdk/tree/master/packages/walk) — traversing a BEM project's file system
+* [config](https://github.com/bem/bem-sdk/tree/master/packages/config) — keeping all configuration of BEM project in one place
 
 ## Naming
 
-* [@bem/sdk.naming.entity](https://github.com/bem/bem-sdk/tree/master/packages/naming.entity) — the old well-known `bem-naming` package. Contains of `parse` and `stringify` methods (DEPREACTED, use separated packages instead)
-* [@bem/sdk.naming.entity.stringify](https://github.com/bem/bem-sdk/tree/master/packages/naming.entity.stringify) — stringifier for [entity][] representation
-* [@bem/sdk.naming.entity.parse](https://github.com/bem/bem-sdk/tree/master/packages/naming.entity.parse) — parser for [entity][] representation
-* [@bem/sdk.naming.cell.stringify](https://github.com/bem/bem-sdk/tree/master/packages/naming.cell.stringify) — stringifier path to [entity][] inside a BEM project's file system
-* [@bem/sdk.naming.presets](https://github.com/bem/bem-sdk/tree/master/packages/naming.presets) — well-known presets for `naming.*` packages
+* [naming.entity](https://github.com/bem/bem-sdk/tree/master/packages/naming.entity) — the old well-known `bem-naming` package. Contains of `parse` and `stringify` methods — package under consideration, better to use separated packages instead.
+* [naming.entity.stringify](https://github.com/bem/bem-sdk/tree/master/packages/naming.entity.stringify) — stringifier for [entity][] representation
+* [naming.entity.parse](https://github.com/bem/bem-sdk/tree/master/packages/naming.entity.parse) — parser for [entity][] representation
+* [naming.cell.stringify](https://github.com/bem/bem-sdk/tree/master/packages/naming.cell.stringify) — stringifier path to [entity][] inside a BEM project's file system
+* [naming.cell.match](https://github.com/bem/bem-sdk/tree/master/packages/naming.cell.match) — loose parser for [entity][] path to [entity][] inside a BEM project's file system
+* [naming.presets](https://github.com/bem/bem-sdk/tree/master/packages/naming.presets) — well-known presets for `naming.*` packages
 
 ## Declarations
 
-* [@bem/sdk.decl](https://github.com/bem/bem-sdk/tree/master/packages/decl) — working with sets of [entity][], intersecting, merging, substracting...
-* [@bem/sdk.bemjson-to-decl](https://github.com/bem/bem-sdk/tree/master/packages/bemjson-to-decl) – extracting declarations from [bemjson][]
-* [@bem/sdk.bemjson-to-jsx](https://github.com/bem/bem-sdk/tree/master/packages/bemjson-to-jsx) – transforming [bemjson] to [JSX][] markup
-* [@bem/sdk.import-notation](https://github.com/bem/bem-sdk/tree/master/packages/import-notation) — extracting declarations from es6-import/require strings
+* [decl](https://github.com/bem/bem-sdk/tree/master/packages/decl) — working with sets of [entity][], intersecting, merging, substracting...
+* [bemjson-to-decl](https://github.com/bem/bem-sdk/tree/master/packages/bemjson-to-decl) – extracting declarations from [bemjson][]
+* [bemjson-to-jsx](https://github.com/bem/bem-sdk/tree/master/packages/bemjson-to-jsx) – transforming [bemjson][] to [JSX][] markup
+* [import-notation](https://github.com/bem/bem-sdk/tree/master/packages/import-notation) — extracting declarations from es6-import and require strings
 
 ## Dependencies
 
-* [@bem/sdk.graph](https://github.com/bem/bem-sdk/tree/master/packages/graph) — working with graph of dependencies
-* [@bem/sdk.deps](https://github.com/bem/bem-sdk/tree/master/packages/deps) — high-level tool for managing bem-deps
+* [graph](https://github.com/bem/bem-sdk/tree/master/packages/graph) — working with graph of dependencies
+* [deps](https://github.com/bem/bem-sdk/tree/master/packages/deps) — high-level tool for managing [BEM][]-based deps
 
 ## Containers
 
-* [@bem/sdk.entity-name](https://github.com/bem/bem-sdk/tree/master/packages/entity-name) — [entity][] name representation
-* [@bem/sdk.cell](https://github.com/bem/bem-sdk/tree/master/packages/cell) — partial [entity][] with tech and layer
-* [@bem/sdk.file](https://github.com/bem/bem-sdk/tree/master/packages/file) — partial cell with path and level
-* [@bem/sdk.bundle](https://github.com/bem/bem-sdk/tree/master/packages/bundle) — representation of bem-bundle
+* [entity-name](https://github.com/bem/bem-sdk/tree/master/packages/entity-name) — [entity][] name representation
+* [cell](https://github.com/bem/bem-sdk/tree/master/packages/cell) — partial [entity][] with tech and layer
+* [file](https://github.com/bem/bem-sdk/tree/master/packages/file) — partial cell with full path and level
+* [bundle](https://github.com/bem/bem-sdk/tree/master/packages/bundle) — representation of [BEM][] bundles: name, set of cells, and bemjson optionally
 
 [BEM]: https://en.bem.info
 [entity]: https://en.bem.info/methodology/key-concepts/#bem-entity
---
+++ packages/walk/lib/walkers/sdk.js
@@ -0,0 +1,295 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const each = require('async-each');
+const BemFile = require('@bem/sdk.file');
+const createPreset = require('@bem/sdk.naming.presets/create');
+const createParse = require('@bem/sdk.naming.entity.parse');
+const createStringify = require('@bem/sdk.naming.entity.stringify');
+const createMatch = require('@bem/sdk.naming.cell.match');
+
+/**
+ * Calls specified callback for each file or directory in specified directory.
+ *
+ * Each item is object with the following fields:
+ * * {string} path — the absolute path to file or directory.
+ * * {string} basename — the name of file or directory (the last portion of a path).
+ * * {string} stem - the name without tech name (complex extention).
+ * * {string} tech - the complex extention for the file or directory path.
+ *
+ * @param {string}   dirname — the path to directory.
+ * @param {function} fn — the function that is called on each file or directory.
+ * @param {function} callback — the callback function.
+ */
+const eachDirItem = (dirname, fn, callback) => {
+    fs.readdir(dirname, (err, filenames) => {
+        if (err) {
+            return callback(err);
+        }
+
+        const files = filenames.map(basename => {
+            const dotIndex = basename.indexOf('.');
+
+            // has tech
+            if (dotIndex > 0) {
+                return {
+                    path: path.join(dirname, basename),
+                    basename: basename,
+                    stem: basename.substring(0, dotIndex),
+                    tech: basename.substring(dotIndex + 1)
+                };
+            }
+
+            return {
+                path: path.join(dirname, basename),
+                basename: basename,
+                stem: basename
+            };
+        });
+
+        each(files, fn, callback);
+    });
+};
+
+/**
+ * Helper to scan one level.
+ */
+class LevelWalker {
+//     /**
+//      * @param {object}        info        The info about scaned level.
+//      * @param {string}        info.path   The level path to scan.
+//      * @param {object|string} info.naming The naming options.
+//      * @param {function}      add         The function to provide info about found files.
+//      */
+//     constructor (info, add) {
+//         this.levelpath = info.path;
+
+//         const preset = createPreset(info.naming);
+//         // Create `@bem/sdk.naming` instance for specified options.
+//         this.naming = {
+//             parse: createParse(preset),
+//             stringify: createStringify(preset)
+//         };
+
+//         this.add = add;
+//     }
+    /**
+     * Scans the block directory.
+     *
+     * @param {string} dirname - the path to directory of block.
+     * @param {string} blockname - the name of block.
+     * @param {function} callback — the callback function.
+     */
+    scanBlockDir (dirname, blockname, callback) {
+        eachDirItem(dirname, (item, cb) => {
+            const filename = item.path;
+            const stem = item.stem;
+            const tech = item.tech;
+
+            if (tech) {
+                if (blockname === stem) {
+                    this.add(new BemFile({
+                        cell: {
+                            block: blockname,
+                            tech: tech,
+                            layer: null
+                        },
+                        level: this.levelpath,
+                        path: filename
+                    }));
+                }
+
+                return cb();
+            }
+
+            const entity = this.naming.parse(blockname + stem);
+            const type = entity && entity.type;
+
+            if (type === 'blockMod') {
+                return this.scanBlockModDir(filename, entity, cb);
+            }
+
+            if (type === 'elem') {
+                return this.scanElemDir(filename, entity, cb);
+            }
+
+            cb();
+        }, callback);
+    }
+    /**
+     * Scans the modifier of block directory.
+     *
+     * @param {string} dirname - the path to directory of modifier.
+     * @param {object} scope - the entity object for modifier.
+     * @param {function} callback — the callback function.
+     */
+    scanBlockModDir (dirname, scope, callback) {
+        eachDirItem(dirname, (item, cb) => {
+            const entity = this.naming.parse(item.stem);
+            const tech = item.tech;
+
+            // Find file with same modifier name.
+            if (tech && entity && scope.block === entity.block
+                && scope.mod.name === (entity.mod && entity.mod.name)) {
+                this.add(new BemFile({
+                    cell: {
+                        entity: entity,
+                        tech: tech,
+                        layer: null
+                    },
+                    level: this.levelpath,
+                    path: item.path
+                }));
+            }
+
+            cb();
+        }, callback);
+    }
+    /**
+     * Scans the element directory.
+     *
+     * @param {string} dirname - the path to directory of element.
+     * @param {object} scope - the entity object for element.
+     * @param {function} callback — the callback function.
+     */
+    scanElemDir (dirname, scope, callback) {
+        eachDirItem(dirname, (item, cb) => {
+            const filename = item.path;
+            const stem = item.stem;
+            const tech = item.tech;
+
+            if (tech) {
+                // Find file with same element name.
+                if (this.naming.stringify(scope) === stem) {
+                    const entity = this.naming.parse(stem);
+
+                    this.add(new BemFile({
+                        cell: {
+                            entity: entity,
+                            tech: tech,
+                            layer: null
+                        },
+                        level: this.levelpath,
+                        path: item.path
+                    }));
+                }
+
+                return cb();
+            }
+
+            const entity = this.naming.parse(scope.block + path.basename(dirname) + stem);
+            const type = entity && entity.type;
+
+            if (type === 'elemMod') {
+                return this.scanElemModDir(filename, entity, cb);
+            }
+
+            cb();
+        }, callback);
+    }
+    /**
+     * Scans the modifier of element directory.
+     *
+     * @param {string} dirname - the path to directory of modifier.
+     * @param {object} scope - the entity object for modifier.
+     * @param {function} callback — the callback function.
+     */
+    scanElemModDir (dirname, scope, callback) {
+        eachDirItem(dirname, (item, cb) => {
+            const entity = this.naming.parse(item.stem);
+            const tech = item.tech;
+
+            // Find file with same modifier name.
+            if (tech && entity
+                && scope.block === entity.block
+                && scope.elem === entity.elem
+                && scope.mod.name === (entity.mod && entity.mod.name)
+            ) {
+                this.add(new BemFile({
+                    cell: {
+                        entity: entity,
+                        tech: tech,
+                        layer: null
+                    },
+                    level: this.levelpath,
+                    path: item.path
+                }));
+            }
+
+            cb();
+        }, callback);
+    }
+}
+
+/**
+ * Scans the block directory.
+ *
+ * @param {string} dirname - the path to directory of block.
+ * @param {string} blockname - the name of block.
+ * @param {function} callback — the callback function.
+ */
+const deeperInDir = (dirname, blockname, callback) => {
+    eachDirItem(dirname, (item, cb) => {
+        const filename = item.path;
+        const stem = item.stem;
+        const tech = item.tech;
+
+        if (tech) {
+            if (blockname === stem) {
+                this.add(new BemFile({
+                    cell: {
+                        block: blockname,
+                        tech: tech,
+                        layer: null
+                    },
+                    level: this.levelpath,
+                    path: filename
+                }));
+            }
+
+            return cb();
+        }
+
+        const entity = this.naming.parse(blockname + stem);
+        const type = entity && entity.type;
+
+        if (type === 'blockMod') {
+            return this.scanBlockModDir(filename, entity, cb);
+        }
+
+        if (type === 'elem') {
+            return this.scanElemDir(filename, entity, cb);
+        }
+
+        cb();
+    });
+}
+
+/**
+ * Plugin to scan nested levels.
+ *
+ * @param {object}        info        The info about scaned level.
+ * @param {string}        info.path   The level path to scan.
+ * @param {function}      add         The function to provide info about found files.
+ * @param {function}      callback    The callback function.
+ */
+module.exports = (info, add, callback) => {
+    // console.log(info);
+    // const walker = new LevelWalker(info, add);
+
+    console.log(info.path);
+    // Scan level
+    eachDirItem(info.path, (item, cb) => {
+        console.log(item);
+        const matchResult = match(item);
+
+        if (item) {
+            deeperInDir(item.path, item.basename, cb);
+            return;
+        }
+
+        cb();
+    });
+};
