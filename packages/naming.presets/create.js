342b21b782a272457c404aaaa0dae8f7ec9af3a1
Mikhail Goncharov <migs911@yandex.ru>
2018-11-29 20:27:17 +0300

    Added detailed description for init function in the naming.presets

--- packages/naming.presets/create.js
+++ packages/naming.presets/create.js
@@ -6,12 +6,25 @@ var DEFAULT_PRESET = 'origin';
 
 module.exports = init;
 
-/**
- * Returns delims and wordPattern.
+ /**
+ * Returns an object with `delims`, `fs` and `wordPattern` properties
+ * that describes the naming convention.
  *
- * @param {Object} options - user options
- * @param {Object} [userDefaults] - defaults
- * @returns {INamingConventionDelims}
+ * @param {(Object|string)} [options] — user options or preset name.
+ *                                      If not specified, default preset will be returned.
+ * @param {string} [options.preset] — preset name that should be used as default preset.
+ * @param {Object} [options.delims] — strings to separate names of bem entities.
+ *                                    This object has the same structure with `INamingConventionDelims`,
+ *                                    but all properties inside are optional.
+ * @param {Object} [options.fs] — user options to separate names of files with bem entities.
+ * @param {Object} [options.fs.delims] — strings to separate names of files in a BEM project.
+ *                                       This object has the same structure with `INamingConventionDelims`,
+ *                                       but all properties inside are optional.
+ * @param {string} [options.fs.pattern] — pattern that describes the file structure of a BEM project.s
+ * @param {string} [options.fs.scheme] — schema name that describes the file structure of one BEM entity.
+ * @param {string} [options.wordPattern] — a regular expression that will be used to match an entity name.
+ * @param {(Object|string)} [userDefaults] — default options that will override the options from default preset.
+ * @returns {INamingConvention}
  */
 function init(options, userDefaults) {
     if (!options) {
@@ -45,7 +58,7 @@ function init(options, userDefaults) {
 
     const res = {
         delims: {
-            elem: optionsDelims.elem || userDefaults.elim || defaultDelims.elem,
+            elem: optionsDelims.elem || userDefaults.elem || defaultDelims.elem,
             mod: typeof mod === 'string'
                 ? { name: mod, val: mod }
                 : {
